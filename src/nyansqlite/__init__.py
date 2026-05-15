c2d49a837268eb30ea281bbdd64887fcaba5ee9a
harumaki4649 <support@disnana.com>
2026-05-15 18:21:18 +0900

    no message

--- src/nyansqlite/__init__.py
+++ src/nyansqlite/__init__.py
@@ -3,3 +3,4 @@ from ._markers import CompositeIndex, Indexed, Searchable, UniqueIndexed
 from .exceptions import FieldNotFoundError, ModelNotRegisteredError, SearchNotEnabledError
 
 __all__ = ["NyanSQLite"]
+__version__ = "1.0.0dev1"
